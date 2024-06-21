6f708abd613e052da159a8e50f1f6f0f3ab5056c
ftmhabt <ftmh.a78@gmail.com>
2024-06-21 12:54:54 +0330

    Add search page

---
+++ app/search/page.tsx
@@ -0,0 +1,9 @@
+import Searchbar from "../ui/search/searchbar";
+
+export default function Page() {
+  return (
+    <div>
+        <Searchbar/>
+    </div>
+  )
+}
---
+++ app/ui/search/searchbar.tsx
@@ -0,0 +1,9 @@
+import Search from "../general/search";
+
+export default function Searchbar() {
+  return (
+    <div className="flex items-center justify-center p-[1rem] bg-amber-600 max-w-[900px] mx-auto">
+        <Search/>
+    </div>
+  )
+}
