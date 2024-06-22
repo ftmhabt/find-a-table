c5bd2c8d648ba6be353c904199b7ea4c453b5185
ftmhabt <ftmh.a78@gmail.com>
2024-06-22 12:31:19 +0330

    Add dish card

---
+++ app/ui/restaurant/dish-card.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function DishCard({
+    name,
+    description,
+    price
+}:{
+    name:string,
+    description:string,
+    price:Number
+}) {
+  return (
+    <div className='flex flex-col gap-2 bg-white p-4'>
+        <h1 className='text-xl font-bold'>{name}</h1>
+        <p>{description}</p>
+        <p>{price.toString()}$</p>
+    </div>
+  )
+}
--- app/ui/restaurant/menu-card.tsx
+++ app/ui/restaurant/menu-card.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import RestaurantBar from './restaurant-bar'
+import React from "react";
+import RestaurantBar from "./restaurant-bar";
+import DishCard from "./dish-card";
 
 export default function MenuCard() {
   return (
-    <div className='max-w-[823px] bg-slate-200 -mt-10 ml-10'>
-        <RestaurantBar/>
+    <div className="max-w-[823px] bg-slate-200 -mt-10 ml-10">
+      <RestaurantBar />
+      <div className="p-4 flex flex-col gap-4">
+        <h1 className="text-2xl">Menu</h1>
+        <DishCard
+          name="totot"
+          description="Compiled in 165ms (270 modules)"
+          price={50}
+        />
+      </div>
     </div>
-  )
+  );
 }
