a9b3c39fe954c9bf5fe4a93d907172bcfd86d656
ftmhabt <ftmh.a78@gmail.com>
2024-06-22 12:22:26 +0330

    Fix style

--- app/ui/restaurant/menu-card.tsx
+++ app/ui/restaurant/menu-card.tsx
@@ -3,7 +3,7 @@ import RestaurantBar from './restaurant-bar'
 
 export default function MenuCard() {
   return (
-    <div className='w-[823px]'>
+    <div className='max-w-[823px] bg-slate-200 -mt-10 ml-10'>
         <RestaurantBar/>
     </div>
   )
