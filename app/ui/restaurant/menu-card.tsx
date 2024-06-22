487cfd78e0a7e60659b10585924719b73735e786
ftmhabt <ftmh.a78@gmail.com>
2024-06-22 12:15:09 +0330

    Add menu card for restaurant

---
+++ app/ui/restaurant/menu-card.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RestaurantBar from './restaurant-bar'
+
+export default function MenuCard() {
+  return (
+    <div className='w-[823px]'>
+        <RestaurantBar/>
+    </div>
+  )
+}
