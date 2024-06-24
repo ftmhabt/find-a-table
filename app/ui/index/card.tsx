8aa6aa909aa608ca5b560d6001728ac81a560d26
ftmhabt <ftmh.a78@gmail.com>
2024-06-25 01:04:53 +0330

    Show fetched data on cards

--- app/ui/index/card.tsx
+++ app/ui/index/card.tsx
@@ -1,17 +1,30 @@
+import { Cuisine, Location, PRICE } from "@prisma/client";
 import Image from "next/image";
 
-export default function Card() {
+export interface RestaurantType {
+  id:Number,
+  name:string,
+  main_image:string,
+  cuisine:Cuisine,
+  location:Location,
+  price:PRICE
+}
+
+export default function Card({restaurant}:{restaurant:RestaurantType}) {
   return (
     <div className="w-[200px] shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
-      <Image
-        src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        width={200}
+      <img
+        src={restaurant.main_image}
         height={120}
-        alt="name"
-        unoptimized={true}
+        alt={restaurant.name}
       />
       <div className="flex flex-col gap-2 p-2">
-        <h1>name</h1>
+        <h1>{restaurant.name}</h1>
+        <div>
+        <div>{restaurant.cuisine.name}</div>
+        <div>{restaurant.location.name}</div>
+        <div>{restaurant.price}</div>
+        </div>
         <div>booked 3 times today</div>
       </div>
     </div>
