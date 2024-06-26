03160e9d90d53e65939c65c05a1cf16037a69c7f
ftmhabt <ftmh.a78@gmail.com>
2024-06-26 19:51:50 +0330

    Add hero to manu page

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -4,6 +4,9 @@ import MenuCard from "../../../ui/restaurant/menu-card";
 export default function restaurantMenu() {
   return (
     <div>
+      <Hero heightClass="h-[300px]">
+        <h1>restaurant name</h1>
+      </Hero>
       <MenuCard/>
     </div>
   );
