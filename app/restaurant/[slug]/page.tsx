7bbec98b342b85d4c5110cc0ca854f3e2405adfd
ftmhabt <ftmh.a78@gmail.com>
2024-06-26 19:43:52 +0330

    Fetch restaurant page data by slug

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,20 +1,54 @@
+import { PrismaClient } from "@prisma/client";
 import Hero from "../../ui/general/hero";
 import ReserveCard from "../../ui/restaurant/reserve-card";
 import RestaurantDetailsCard from "../../ui/restaurant/restaurant-card";
+import { error } from "console";
 
-export default function restaurantDatails() {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: Number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+async function fetchRestaurantBySlug(slug: string): Promise<Restaurant> {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant;
+}
+
+export default async function restaurantDatails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const resaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <div>
       <RestaurantDetailsCard
-        name="rest"
-        description="kdfhjdshafeiwahlw"
-        photos={[
-          "https://images.pexels.com/photos/67468/pexels-photo-67468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-          "https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-          "https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        ]}
+        name={resaurant.name}
+        description={resaurant.description}
+        photos={resaurant.images.map((image) => image)}
       />
-      <ReserveCard/>
+      <ReserveCard />
     </div>
   );
 }
