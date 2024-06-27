86c871837b6da0cd2ad09c7c73ba5b05ec895977
ftmhabt <ftmh.a78@gmail.com>
2024-06-27 22:04:48 +0330

    Replace placeholders with data

--- app/ui/restaurant/menu-card.tsx
+++ app/ui/restaurant/menu-card.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import RestaurantBar from "./restaurant-bar";
 import DishCard from "./dish-card";
+import { Item } from "@prisma/client";
 
-export default function MenuCard() {
+export default function MenuCard({ menu }: { menu: Item[] }) {
   return (
     <div className="max-w-[823px] bg-slate-200 -mt-10 ml-10">
       <RestaurantBar />
       <div className="p-4 flex flex-col gap-4">
         <h1 className="text-2xl">Menu</h1>
-        <DishCard
-          name="totot"
-          description="Compiled in 165ms (270 modules)"
-          price={50}
-        />
+        {menu.map((item) => (
+          <DishCard
+            key={item.id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+          />
+        ))}
       </div>
     </div>
   );
