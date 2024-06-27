ed87a749635c6e72b8d66a84c554e540a2228208
ftmhabt <ftmh.a78@gmail.com>
2024-06-27 22:04:08 +0330

    Fetch menu data

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,10 +1,33 @@
-import Hero from "../../../ui/general/hero";
+import { PrismaClient } from "@prisma/client";
 import MenuCard from "../../../ui/restaurant/menu-card";
 
-export default function restaurantMenu() {
+const prisma = new PrismaClient();
+
+async function fetchRestaurantMenu(slug: string) {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      items: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant.items;
+}
+export default async function restaurantMenu({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const items = await fetchRestaurantMenu(params.slug);
   return (
     <div>
-      <MenuCard/>
+      <MenuCard menu={items} />
     </div>
   );
 }
