73f4d8f2a7dc8cb004d1a66157dd3ac356f397aa
ftmhabt <ftmh.a78@gmail.com>
2024-06-28 16:52:30 +0330

    Handle search and routing

--- app/ui/general/search.tsx
+++ app/ui/general/search.tsx
@@ -1,15 +1,32 @@
-'use client';
+"use client";
+import { useState } from "react";
 import Button from "./button";
+import { useRouter } from "next/navigation";
 
 export default function Search() {
+  const [location, setLocation] = useState("");
+  const router = useRouter();
   return (
     <div className="flex">
-        <input type="text" name="" id="" />
-        <Button text="lets go" onClick={onSearch} style="bg-black px-[1rem] py-[.25rem] text-white"/>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="State, city or town"
+        value={location}
+        onChange={(e) => {
+          setLocation(e.target.value);
+        }}
+      />
+      <Button
+        text="lets go"
+        onClick={() => {
+          if (location === "") return;
+          router.push(`/search?city=${location}`);
+          setLocation('');
+        }}
+        style="bg-black px-[1rem] py-[.25rem] text-white"
+      />
     </div>
-  )
-}
-
-function onSearch(){
-    return ;
+  );
 }
