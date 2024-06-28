4aa0699de9bad16d9ad03aa84c4118ebe6c863e6
ftmhabt <ftmh.a78@gmail.com>
2024-06-28 16:03:12 +0330

    Add no menu message

--- app/ui/restaurant/menu-card.tsx
+++ app/ui/restaurant/menu-card.tsx
@@ -3,20 +3,30 @@ import RestaurantBar from "./restaurant-bar";
 import DishCard from "./dish-card";
 import { Item } from "@prisma/client";
 
-export default function MenuCard({ menu,slug }: { menu: Item[] ,slug:string}) {
+export default function MenuCard({
+  menu,
+  slug,
+}: {
+  menu: Item[];
+  slug: string;
+}) {
   return (
     <div className="max-w-[823px] bg-slate-200 -mt-10 ml-10">
-      <RestaurantBar slug={slug}/>
+      <RestaurantBar slug={slug} />
       <div className="p-4 flex flex-col gap-4">
         <h1 className="text-2xl">Menu</h1>
-        {menu.map((item) => (
-          <DishCard
-            key={item.id}
-            name={item.name}
-            description={item.description}
-            price={item.price}
-          />
-        ))}
+        {menu.length > 0 ? (
+          menu.map((item) => (
+            <DishCard
+              key={item.id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+            />
+          ))
+        ) : (
+          <p>this restaurant doesnt have a menu</p>
+        )}
       </div>
     </div>
   );
