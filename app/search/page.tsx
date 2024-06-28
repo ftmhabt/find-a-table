a88109484f2703bada1c75fc35aa65f8c2b125f4
ftmhabt <ftmh.a78@gmail.com>
2024-06-28 17:02:03 +0330

    Get search params and show on page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,10 +1,18 @@
+"use client";
 import Hero from "../ui/general/hero";
 import Search from "../ui/general/search";
+import { useSearchParams } from "next/navigation";
 
 export default function SearchPage() {
+  const searchParams = useSearchParams();
+  const city = searchParams?.get("city");
+
   return (
-    <Hero  heightClass="h-[50px]">
-      <Search />
-    </Hero>
+    <div>
+      <Hero heightClass="h-[50px]">
+        <Search />
+      </Hero>
+      <div>{city}</div>
+    </div>
   );
 }
