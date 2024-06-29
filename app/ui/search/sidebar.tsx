033fb0918260137c1a6dcf5ab00152d533b18aaf
ftmhabt <ftmh.a78@gmail.com>
2024-06-29 23:16:13 +0330

    Show locations on sidebar

--- app/ui/search/sidebar.tsx
+++ app/ui/search/sidebar.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import { PrismaClient } from "@prisma/client";
 
-export default function Sidebar() {
+const prisma = new PrismaClient();
+
+async function getLocations() {
+  return await prisma.location.findMany();
+}
+export default async function Sidebar() {
+  const locations = await getLocations();
   return (
     <div>
-        <h1>Location</h1>
-        <ul>
-            <li></li>
-        </ul>
-        <h1>Cuisine</h1>
-        <ul>
-            <li></li>
-        </ul>
+      <h1>Location</h1>
+      <ul>
+        {locations.map((location) => (
+          <li key={location.id}>{location.name}</li>
+        ))}
+      </ul>
+      <h1>Cuisine</h1>
+      <ul>
+        <li></li>
+      </ul>
     </div>
-  )
+  );
 }
