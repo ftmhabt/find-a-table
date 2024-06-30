20d37719163471fe5509bda2043f01b8aa32688a
ftmhabt <ftmh.a78@gmail.com>
2024-06-30 23:53:37 +0330

    Fix position

--- app/ui/restaurant/reserve-card.tsx
+++ app/ui/restaurant/reserve-card.tsx
@@ -4,7 +4,7 @@ import Button from "../general/button";
 
 export default function ReserveCard() {
   return (
-    <div className="flex flex-col gap-3 bg-white p-4 w-[280px] fixed top-[300px] right-[390px] shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
+    <div className="flex flex-col gap-3 bg-white p-4 w-[280px] fixed top-[300px] left-[60%] shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]">
         <h1>make a reservation</h1>
         <label htmlFor="size">party size</label>
         <select name="size" id="size">
