ee6f0f4c0f65c8dfa094fadd022361a27e8097e9
ftmhabt <ftmh.a78@gmail.com>
2024-07-02 23:21:28 +0330

    Change button color

--- app/ui/general/link-button.tsx
+++ app/ui/general/link-button.tsx
@@ -6,5 +6,5 @@ export default function LinkButton({
   text: string;
   url: string;
 }) {
-  return <Link className="ml-2 bg-orange-300 rounded px-4 py-1 hover:bg-orange-400 transition-all" href={url}>{text}</Link>;
+  return <Link className="ml-2 bg-sky-600 rounded px-4 py-1 hover:bg-sky-400 transition-all text-white" href={url}>{text}</Link>;
 }
