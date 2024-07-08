c0955e5ac5679336e0e4262fe332efcb7bcd04d6
ftmhabt <ftmh.a78@gmail.com>
2024-07-08 22:07:50 +0330

    Fix function to correctly show decimal numbers

--- app/utils/calculate-rating-average.ts
+++ app/utils/calculate-rating-average.ts
@@ -2,5 +2,6 @@ import { Review } from "@prisma/client";
 
 export function CalculateRatingAverage(reviews: Review[]) {
     if (!reviews.length) return 0;
-    return reviews.reduce((sum, review) => sum + review.rating, 0) / reviews.length
+    const rating = reviews.reduce((sum, review) => sum + review.rating, 0) / reviews.length;
+    return rating % 1 === 0 ? rating : rating.toFixed(1);
 }
