9df5946c5d634eb3eba151a4bdd187936c35c063
ftmhabt <ftmh.a78@gmail.com>
2024-07-09 19:33:19 +0330

    Fix type issue

--- app/utils/calculate-rating-average.ts
+++ app/utils/calculate-rating-average.ts
@@ -3,5 +3,5 @@ import { Review } from "@prisma/client";
 export function CalculateRatingAverage(reviews: Review[]) {
     if (!reviews.length) return 0;
     const rating = reviews.reduce((sum, review) => sum + review.rating, 0) / reviews.length;
-    return rating % 1 === 0 ? rating : rating.toFixed(1);
+    return rating % 1 === 0 ? rating : parseFloat(rating.toFixed(1));
 }
