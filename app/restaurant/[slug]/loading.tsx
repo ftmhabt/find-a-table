fc56533c308ba016d354a638f336246d04558845
ftmhabt <ftmh.a78@gmail.com>
2024-07-11 19:20:49 +0330

    Add loading page for restaurant page

---
+++ app/restaurant/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+import Hero from "../../ui/general/hero";
+
+export default function loading() {
+  return (
+    <div>
+      <div className="max-w-[500px] h-screen bg-slate-200 -mt-10 ml-10"></div>
+    </div>
+  )
+}
