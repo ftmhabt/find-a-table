48c9b4d49bf1bd4b6a364560fe3ddd19a28cf212
ftmhabt <ftmh.a78@gmail.com>
2024-07-12 19:49:30 +0330

    Fix props

--- app/ui/general/button.tsx
+++ app/ui/general/button.tsx
@@ -1,11 +1,9 @@
 export default function Button({
   text,
   onClick,
-  style
 }: {
   text: string;
   onClick: () => void;
-  style: string;
 }) {
-  return <button onClick={onClick} className={style}>{text}</button>;
+  return <button onClick={onClick} className="ml-2 bg-sky-600 rounded px-4 py-1 hover:bg-sky-400 transition-all text-white">{text}</button>;
 }
