92cd37b45d82367581821d812b19cc184336d5e1
ftmhabt <ftmh.a78@gmail.com>
2024-07-12 18:11:00 +0330

    Add sign up modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import Header from "./ui/general/header";
 import "../styles/globals.css";
 import Head from "next/head";
-import { Metadata } from 'next'
- 
+import { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: {
-    template: '%s | Find a table',
-    default: 'Find a table', 
+    template: "%s | Find a table",
+    default: "Find a table",
   },
-}
+};
 
 export default function RootLayout({
   children,
@@ -17,9 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="max-w-[900px] min-h-[100vh] mx-auto">
-        <Header />
-        {children}
+      <body className=" m-0 p-0">
+        <div className="max-w-[900px] min-h-[100vh] mx-auto">
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
   );
--- app/ui/general/header.tsx
+++ app/ui/general/header.tsx
@@ -1,12 +1,13 @@
 import Logo from "./logo";
 import LinkButton from "./link-button";
+import SignUpModal from "../auth/signup";
 
 export default function Header() {
   return (
     <div className="flex py-1">
         <Logo/>
         <LinkButton text="sign in" url=''/>
-        <LinkButton text="sign up" url=''/>
+        <SignUpModal/>
     </div>
   )
 }
