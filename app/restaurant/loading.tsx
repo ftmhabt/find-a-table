2f34fdfbee9743d843b95f2780998cb4a1b2c744
ftmhabt <ftmh.a78@gmail.com>
2024-07-17 23:36:51 +0330

    Fix image element

--- app/restaurant/loading.tsx
+++ app/restaurant/loading.tsx
@@ -1,10 +1,11 @@
 import spinner from '../icons/spinner.png'
+import Image from 'next/image'
 
 
 export default function loading() {
   return (
     <div className="flex items-center justify-center h-[80vh]">
-      <img src={spinner} alt="loading" />
+      <Image src={spinner} alt='loading' />
     </div>
   )
 }
