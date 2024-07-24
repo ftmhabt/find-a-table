994e3d67b9ef1a4b0c0c3c033bf2855720361292
ftmhabt <ftmh.a78@gmail.com>
2024-07-24 23:03:33 +0330

    Add middleware

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
-import * as jose from 'jose';
 import jwt from 'jsonwebtoken'
 
 
@@ -9,39 +8,21 @@ const prisma = new PrismaClient();
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
 
-    const bearerToken=req.headers["authorization"] as string
+    const bearerToken = req.headers["authorization"] as string
 
-    if(!bearerToken){
+    const token = bearerToken.split(" ")[1];
+
+    const payload = jwt.decode(token) as { email: string }
+
+    if (!payload.email) {
         return res.status(401).json({ errorMessage: "unauthorized" })
     }
 
-    const token =bearerToken.split(" ")[1];
-
-    if(!token)
-    {
-        return res.status(401).json({ errorMessage: "unauthorized" })
-    }
-
-    const secret =new TextEncoder().encode(process.env.JWT_SECRET);
-    try{
-        await jose.jwtVerify(token,secret)
-    }
-    catch(error){
-        return res.status(401).json({ errorMessage: "unauthorized" })
-    
-    }
-
-    const payload=jwt.decode(token) as {email:string}
-
-    if(!payload.email){
-        return res.status(401).json({ errorMessage: "unauthorized" })
-    }
-
-    const user =await prisma.user.findUnique({
-        where:{
-            email:payload.email
+    const user = await prisma.user.findUnique({
+        where: {
+            email: payload.email
         }
     })
 
-    return res.status(200).json({user});
+    return res.status(200).json({ user });
 }
