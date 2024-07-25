c3b5b64d126df4ae71c5ea26ca3c352543364ac3
ftmhabt <ftmh.a78@gmail.com>
2024-07-25 23:49:58 +0330

    Disable form button for null input

--- app/ui/auth/signin-form.tsx
+++ app/ui/auth/signin-form.tsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LoginWrapper from "./login-wrapper";
 import { InputProps } from "./login-modal";
 
 export default function SigninForm({ props }: { props: InputProps }) {
   const { inputs, handleChange } = props;
+  const [disabled,setDisabled]=useState(true);
+
+  useEffect(()=>{
+    if(inputs.email && inputs.password)
+    {
+      setDisabled(false);
+    }
+    else{
+      setDisabled(true);
+    }
+  },[inputs])
   return (
     <LoginWrapper>
       <div>welcome</div>
       <div>sign in</div>
       <input type="email" name="email" id="email" placeholder="email" value={inputs.email} onChange={(e) => handleChange(e)} />
       <input type="password" name="password" id="password" placeholder="password" value={inputs.password} onChange={(e) => handleChange(e)} />
-      <button>submit</button>
+      <button disabled={disabled} className="disabled:bg-gray-500">submit</button>
     </LoginWrapper>
   );
 }
--- app/ui/auth/signup-form.tsx
+++ app/ui/auth/signup-form.tsx
@@ -1,8 +1,21 @@
 import LoginWrapper from "./login-wrapper";
 import { InputProps } from "./login-modal";
+import { useEffect, useState } from "react";
 
 export default function SignupForm({ props }: { props: InputProps }) {
   const { inputs, handleChange } = props;
+  const [disabled,setDisabled]=useState(true);
+
+  useEffect(()=>{
+    if(inputs.name && inputs.city && inputs.email && inputs.password && inputs.phone)
+    {
+      setDisabled(false);
+    }
+    else{
+      setDisabled(true);
+    }
+  },[inputs])
+  
   return (
     <LoginWrapper>
       <div>welcome</div>
@@ -12,7 +25,7 @@ export default function SignupForm({ props }: { props: InputProps }) {
       <input type="email" name="email" id="email" placeholder="email" value={inputs.email} onChange={(e) => handleChange(e)} />
       <input type="text" name="phone" id="phone" placeholder="phone" value={inputs.phone} onChange={(e) => handleChange(e)} />
       <input type="password" name="password" id="password" placeholder="password" value={inputs.password} onChange={(e) => handleChange(e)} />
-      <button>submit</button>
+      <button disabled={disabled} className="disabled:bg-gray-500">submit</button>
     </LoginWrapper>
   );
 }
