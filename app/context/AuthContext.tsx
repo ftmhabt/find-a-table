e0425a7547ab81d6e01f8988f7ab5300c4641b8b
ftmhabt <ftmh.a78@gmail.com>
2024-07-27 22:46:07 +0330

    Create auth context

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+'use client '
+
+import React, { useState, createContext } from "react"
+
+interface User {
+    id: number,
+    name: string,
+    email: string,
+    city: string,
+    phone: string
+}
+interface State {
+    loading: boolean,
+    data: string | null,
+    error: User | null
+}
+
+interface AuthState extends State {
+    setAuthState: React.Dispatch<React.SetStateAction<State>>
+}
+
+export const AuthorizationContext = createContext<AuthState>({
+    loading: false,
+    data: null,
+    error: null,
+    setAuthState: () => { }
+})
+
+export default function AuthContext({ children }: { children: React.ReactNode }) {
+
+    const [authState, setAuthState] = useState<State>({
+        loading: false,
+        data: null,
+        error: null
+    })
+
+    return (
+        <AuthorizationContext.Provider value={{
+            ...authState,
+            setAuthState
+        }}>{children}</AuthorizationContext.Provider>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "./ui/general/header";
 import "../styles/globals.css";
 import Head from "next/head";
 import { Metadata } from "next";
+import AuthContext from "./context/AuthContext";
 
 export const metadata: Metadata = {
   title: {
@@ -18,10 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className=" m-0 p-0">
-        <div className="max-w-[900px] min-h-[100vh] mx-auto">
-          <Header />
-          {children}
-        </div>
+        <AuthContext>
+          <div className="max-w-[900px] min-h-[100vh] mx-auto">
+            <Header />
+            {children}
+          </div>
+        </AuthContext>
       </body>
     </html>
   );
