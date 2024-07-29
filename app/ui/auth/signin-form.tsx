31d2a2a39dae20a74c82c84006a592e998c5d06d
ftmhabt <ftmh.a78@gmail.com>
2024-07-29 22:34:43 +0330

    Handle loading state

--- app/ui/auth/signin-form.tsx
+++ app/ui/auth/signin-form.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import LoginWrapper from "./login-wrapper";
 import { InputProps } from "./login-modal";
 import useAuth from "../../../hooks/useAuth";
+import { AuthorizationContext } from "../../context/AuthContext";
+import loading from '../../icons/spinner.png'
 
 export default function SigninForm({ props }: { props: InputProps }) {
   const { inputs, handleChange } = props;
   const [disabled, setDisabled] = useState(true);
   const { signin } = useAuth();
+  const { data, error, loading, setAuthState } = useContext(AuthorizationContext)
 
   useEffect(() => {
     if (inputs.email && inputs.password) {
@@ -18,11 +21,13 @@ export default function SigninForm({ props }: { props: InputProps }) {
   }, [inputs])
   return (
     <LoginWrapper>
-      <div>welcome</div>
-      <div>sign in</div>
-      <input type="email" name="email" id="email" placeholder="email" value={inputs.email} onChange={(e) => handleChange(e)} />
-      <input type="password" name="password" id="password" placeholder="password" value={inputs.password} onChange={(e) => handleChange(e)} />
-      <button disabled={disabled} className="disabled:bg-gray-500" onClick={() => signin({ email: inputs.email, password: inputs.password })}>submit</button>
+      {loading ? <img src={loading} alt="loading" /> : <>
+        <div>welcome</div>
+        <div>sign in</div>
+        <input type="email" name="email" id="email" placeholder="email" value={inputs.email} onChange={(e) => handleChange(e)} />
+        <input type="password" name="password" id="password" placeholder="password" value={inputs.password} onChange={(e) => handleChange(e)} />
+        <button disabled={disabled} className="disabled:bg-gray-500" onClick={() => signin({ email: inputs.email, password: inputs.password })}>submit</button>
+      </>}
     </LoginWrapper>
   );
 }
