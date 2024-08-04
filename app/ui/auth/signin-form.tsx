834885584d7cc58340865292cb15ea06a48cfd31
ftmhabt <ftmh.a78@gmail.com>
2024-08-04 21:46:30 +0330

    Show error on sign in

--- app/ui/auth/signin-form.tsx
+++ app/ui/auth/signin-form.tsx
@@ -27,6 +27,7 @@ export default function SigninForm({ props }: { props: InputProps }) {
         <input type="email" name="email" id="email" placeholder="email" value={inputs.email} onChange={(e) => handleChange(e)} />
         <input type="password" name="password" id="password" placeholder="password" value={inputs.password} onChange={(e) => handleChange(e)} />
         <button disabled={disabled} className="disabled:bg-gray-500" onClick={() => signin({ email: inputs.email, password: inputs.password })}>submit</button>
+        <h1>{error? error:''}</h1>
       </>}
     </LoginWrapper>
   );
