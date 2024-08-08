14306f85e62d1b6f309abb02954d9809fe80701a
ftmhabt <ftmh.a78@gmail.com>
2024-08-08 17:29:40 +0330

    Make data persistent through reloads

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,43 +1,90 @@
-'use client'
+"use client";
 
-import React, { useState, createContext } from "react"
+import React, { useState, createContext, useEffect } from "react";
+import axios from "axios";
+import { getCookie } from "cookies-next";
 
 interface User {
-    id: number,
-    name: string,
-    email: string,
-    city: string,
-    phone: string
+  id: number;
+  name: string;
+  email: string;
+  city: string;
+  phone: string;
 }
 interface State {
-    loading: boolean,
-    data: User | null,
-    error: string | null
+  loading: boolean;
+  data: User | null;
+  error: string | null;
 }
 
 interface AuthState extends State {
-    setAuthState: React.Dispatch<React.SetStateAction<State>>
+  setAuthState: React.Dispatch<React.SetStateAction<State>>;
 }
 
 export const AuthorizationContext = createContext<AuthState>({
+  loading: false,
+  data: null,
+  error: null,
+  setAuthState: () => {},
+});
+
+export default function AuthContext({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [authState, setAuthState] = useState<State>({
     loading: false,
     data: null,
     error: null,
-    setAuthState: () => { }
-})
+  });
 
-export default function AuthContext({ children }: { children: React.ReactNode }) {
+  const fetchUser = async () => {
+    try {
+      const jwt = getCookie("jwt");
 
-    const [authState, setAuthState] = useState<State>({
+      if (!jwt) {
+        setAuthState({
+          data: null,
+          error: null,
+          loading: false,
+        });
+      }
+
+      const response = await axios.post("http://localhost:3000/api/auth/me", {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+
+      axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+
+      setAuthState({
+        data: response.data,
+        error: null,
         loading: false,
+      });
+    } catch (error: any) {
+      setAuthState({
         data: null,
-        error: null
-    })
+        error: error.response.data.errorMessage,
+        loading: false,
+      });
+    }
+  };
 
-    return (
-        <AuthorizationContext.Provider value={{
-            ...authState,
-            setAuthState
-        }}>{children}</AuthorizationContext.Provider>
-    )
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <AuthorizationContext.Provider
+      value={{
+        ...authState,
+        setAuthState,
+      }}
+    >
+      {children}
+    </AuthorizationContext.Provider>
+  );
 }
