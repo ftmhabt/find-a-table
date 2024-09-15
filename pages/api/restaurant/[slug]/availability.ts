9730f02867458f71d88633331295bd710466f699
ftmhabt <ftmh.a78@gmail.com>
2024-09-15 18:50:53 +0330

    Show available times based on party size

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -87,11 +87,18 @@ export default async function handler(
     });
   });
 
+  const availabilities = searchTimesWithTables.map((t) => {
+    const sumSeats = t.tables.reduce((sum, table) => {
+      return sum + table.seats;
+    }, 0);
+
+    return {
+      time: t.time,
+      available: sumSeats >= parseInt(partySize),
+    };
+  });
+
   return res.json({
-    searchTimes,
-    bookings,
-    bookingTablesObj,
-    tables,
-    searchTimesWithTables,
+    availabilities
   });
 }
