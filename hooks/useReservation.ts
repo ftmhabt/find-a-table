ff8db0d862a042f44d529d6de4b006b15267b65f
ftmhabt <ftmh.a78@gmail.com>
2024-09-30 22:05:05 +0330

    Add reservation hook

---
+++ hooks/useReservation.ts
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function useReservation() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const createReservation = async ({
+    slug,
+    day,
+    time,
+    partySize,
+    bookerName,
+    bookerEmail,
+    bookerPhone,
+    bookerOccasion,
+    bookerRequest,
+  }: {
+    slug: string;
+    day: string;
+    time: string;
+    partySize: number;
+    bookerName: string;
+    bookerEmail: string;
+    bookerPhone: string;
+    bookerOccasion: string;
+    bookerRequest: string;
+  }) => {
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/api/restaurant/${slug}/reserve`,
+        { bookerName, bookerEmail, bookerPhone, bookerOccasion, bookerRequest },
+        {
+          params: {
+            day,
+            time,
+            partySize,
+          },
+        }
+      );
+      setLoading(false);
+      return response.data;
+    } catch (error: any) {
+      setLoading(false);
+      setError(error.response.data.errorMessage);
+    }
+  };
+
+  return { loading, error, createReservation };
+}
