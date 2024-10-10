20ca1e0a8164a7135a5dd567ed44065203fdae20
ftmhabt <ftmh.a78@gmail.com>
2024-10-10 16:25:09 +0330

    Fix cards width

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,6 @@
+import { PiUserCircle } from "react-icons/pi";
 import Hero from "./ui/general/hero";
+import RestaurantContainer from "./ui/general/restaurant-container";
 import Search from "./ui/general/search";
 
 export default function Loading() {
@@ -6,15 +8,16 @@ export default function Loading() {
     <div>
       <Hero>
         <Search />
+        <PiUserCircle size={30} className="text-primary" />
       </Hero>
-      <div className="flex flex-wrap p-6 gap-4">
+      <RestaurantContainer>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
           <div
             key={num}
-            className="animate-pulse bg-slate-200 w-[200px] h-72 rounded overflow-hidden border cursor"
+            className="animate-pulse bg-slate-200 w-72 h-80 rounded overflow-hidden border cursor"
           ></div>
         ))}
-      </div>
+      </RestaurantContainer>
     </div>
   );
 }
